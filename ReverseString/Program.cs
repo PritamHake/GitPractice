c262d55743b3deefa03a914d1e47c4f9675c25f7
TechTornado <TechTornado@DESKTOP-9F65OBU>
2026-02-07 23:33:35 +0530

    I am creating a Reverse String Console

---
+++ ReverseString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Enter a string: ");
+		string name = Console.ReadLine();
+
+		char[] arr = name.ToCharArray();
+		Array.Reverse(arr);
+
+		string reverse = new string(arr);
+
+		Console.WriteLine("Reverse string: " + reverse);
+	}
+}
